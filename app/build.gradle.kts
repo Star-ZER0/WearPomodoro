90d9b2e8eb5ad018c9f3c1afc2b426ff0ccaa9e4
Star_ZER0 <m@star0.cc>
2026-09-20 11:11:13 +0800

    ver: bump version to 0.3.1
    Signed-off-by: Star_ZER0 <m@star0.cc>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "cc.star0.wear.pomodoro"
         minSdk = 25
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
         ndk {
             //noinspection ChromeOsAbiSupport
             abiFilters += listOf("armeabi-v7a", "arm64-v8a")
